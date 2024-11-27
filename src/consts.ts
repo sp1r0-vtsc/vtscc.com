a6eeb253b9f2cad73336d0f969c0cee797e086f0
sp1r0-vtsc <spiro@vtscc.com>
2024-11-27 14:54:50 -0500

    refactored to use astro, added new content

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Site configuration
 export const SITE_TITLE = 'Vermont Secure Computing Consultancy';
 export const SITE_DESCRIPTION = 'Ethan Allen Meets Claude Shannon: Pioneering Simplicity in Decentralized Finance';
+export const SITE_DESCRIPTION = 'Ethan Allen Meets Claude Shannon: Pioneering Simplicity in Decentralized Finance';
 export const COMPANY_EMAIL = 'office [|at|] vtscc.org';
+export const COMPANY_YEAR = '2024';
 export const COMPANY_YEAR = '2024';
